f296006c3d906dc5c27b66373b13b63a823f06e3
May <macbook@Mays-MacBook-Pro.local>
2020-12-09 22:06:36 +0700

    test data in js

--- ClientSeller.js
+++ ClientSeller.js
@@ -6,14 +6,14 @@ var test = document.getElementsByClassName('test');
 var innerCustomerID = document.getElementsByClassName('Check');
 var btnCheck = document.getElementById('btn-check');
 
-var dataTest = [{CustomerID: 0001,
-ListOfFood :[
-    {nameOfFood: "Rice", Amount: 1, Price: "5.000VND"},
-    {nameOfFood: "Noodle", Amount: 2, Price: "10.000VND"},
-    {nameOfFood: "Soup", Amount: 3, Price: "15.000VND"}
-]  
-}
-]
+// var dataTest = [{CustomerID: 0001,
+// ListOfFood :[
+//     {nameOfFood: "Rice", Amount: 1, Price: "5.000VND"},
+//     {nameOfFood: "Noodle", Amount: 2, Price: "10.000VND"},
+//     {nameOfFood: "Soup", Amount: 3, Price: "15.000VND"}
+// ]  
+// }
+// ]
 btnDone.onclick = () => {
     if(listOrder[0].className.split(' ').length == 1){
         listOrder[0].className += ' hidden';
@@ -40,17 +40,48 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 
-readTextFile("./data.json", function(text){
-    data = JSON.parse(text);
-    console.log(dataTest[0].CustomerID);
+// readTextFile("./data.json", function(text){
+//     data = JSON.parse(text);
+//     console.log(dataTest[0].CustomerID);
+// });
+
+// btnCheck.onclick = () => {
+//     let li = document.createElement ("LI");
+//     li.append(data[0].CustomerID);
+//     li.setAttribute('class',`index-text`)
+//     innerCustomerID[0].appendChild(li);
+    
+//     // innerCustomerID.append(li);
+// }
+var data = [{"CustomerID": "0001",
+"ListOfFood" :[
+    {"nameOfFood": "Rice", "Amount": "1", "Price": "5.000VND"},
+    {"nameOfFood": "Noodle", "Amount": "2", "Price": "10.000VND"},
+    {"nameOfFood": "Soup", "Amount": "3", "Price": "15.000VND"}
+]  
+}
+]
+
+var contentNOF = data[0].ListOfFood.map(function(item){
+    return '<div class="Check index-text">' + item.nameOfFood + '</div>';
 });
 
-btnCheck.onclick = () => {
-    let li = document.createElement ("LI");
-    li.append(data[0].CustomerID);
-    li.setAttribute('class',`index-text`)
-    innerCustomerID[0].appendChild(li);
+var contentAmount = data[0].ListOfFood.map(function(item){
+    return '<div class="Check index-text">' + item.Amount + '</div>';
+});
+
+var contentPrice = data[0].ListOfFood.map(function(item){
+    return '<div class="Check index-text">' + item.Price + '</div>';
+});
+
+var htmlList = document.getElementById('todo-List');
+// var contentListOfFood = data[1].map(function(item){
+    //     return '<div class="listOrder index-text"><p>' + item.nameOfFood + '</p></div>';
+    // })
+    var contentCustomerID = data.map(function(item){
+        return '<li><div class="food-order Check"><p class="index-text">' + item.CustomerID + '</p></div>'+ '<div class="food-order">' +  contentNOF + '</div>' +'<div class="food-order">' + contentAmount + '</div>' + '<div class="food-order">' + contentPrice +'</div></li>';
+        
+    });
     
-    // innerCustomerID.append(li);
-}
+    htmlList.innerHTML = contentCustomerID.join('');
 
